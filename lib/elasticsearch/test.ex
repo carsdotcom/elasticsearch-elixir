fbdb24daf5fe9e6960eae275842a965df84301ea
Dairon Medina Caro <dmedinacaro@cars.com>
2025-03-18 18:05:38 -0400

    Delete test module

--- lib/elasticsearch/test.ex
+++
@@ -1,9 +0,0 @@
-defmodule ElasticsearchTest do
-  alias Elasticsearch.Namespace
-
-  def new_process_namespace(pid) when is_pid(pid) do
-    pid
-    |> :erlang.phash2()
-    |> to_string()
-  end
-end
